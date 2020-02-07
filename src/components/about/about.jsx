9ac4ab0531909c7f8c9fb85cef625aac83ca8d2e
xiaole <18434395159@163.com>
2020-02-08 00:12:21 +0800

    更新

---
+++ src/components/about/about.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>高小乐</h2>
+            </div>
+        )
+    }
+}
